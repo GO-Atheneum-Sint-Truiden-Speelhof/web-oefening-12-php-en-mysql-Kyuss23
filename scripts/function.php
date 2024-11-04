4cbb611a1b6983cd05133d10b3a3769f320957d3
Kyuss23 <kyuss.poelmans@atheneumsinttruiden.be>
2024-11-04 11:56:25 +0100

    ConnectToDB functie gemaakt

--- scripts/function.php
+++ scripts/function.php
@@ -1,5 +1,5 @@
 <?php
-    function saveToDB(){
+    function connectToDB(){
         $servername = "localhost";
         $username = "KP23";
         $password = "Kyuss879.";
@@ -10,9 +10,13 @@
 
          // Check connection
          if ($conn->connect_error) {
-         die("Connection failed: " . $conn->connect_error);
+            die("Connection failed: " . $conn->connect_error);
          }
+         return $conn;
 
+    }
+    function saveToDB(){
+        $conn = connectToDB();
         $sql = "INSERT INTO tbl_wedstrijd(NAAM, ADRES, POSTCODE, GEMEENTE, TELEFOONNUMMER, `E-MAILADRES`, GEBOORTEDATUM, `TITEL FOTO`, CAMERA, LENS, `BESCHRIJVING FOTO`)
         VALUES ('".$_POST["naam"]."','".$_POST["adres"]."','".$_POST["postcode"]."','".$_POST["gemeente"]."','".$_POST["telefoonnummer"]."','".$_POST["e-mailadres"]."','".$_POST["geboortedatum"]."','".$_POST["fotoTitel"]."','".$_POST["camera"]."','".$_POST["lens"]."','".$_POST["fotoBeschrijving"]."');";
 
@@ -27,18 +31,5 @@
     }
 
     function printDB(){
-        $servername = "localhost";
-        $username = "KP23";
-        $password = "Kyuss879.";
-        $dbname = "test";
-
-         //create connection
-         $conn  = new mysqli($servername, $username, $password, $dbname);
-
-         // Check connection
-         if ($conn->connect_error) {
-         die("Connection failed: " . $conn->connect_error);
-         }
-         
     }
 ?>
